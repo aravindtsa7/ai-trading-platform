a67f0030cc5f0e815a84d65869859c90a9fef450
aravindtsa7 <aravindtsa@gmail.com>
2026-07-24 13:14:34 +0530

    feat: add async handler utility

--- src/routes/test.routes.ts
+++ src/routes/test.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { AppError } from "../common/exceptions";
+import { asyncHandler } from "../common/helpers";
 
 const router = Router();
 
-router.get("/error", () => {
-  throw new AppError("This is a test error", 400);
-});
+router.get(
+  "/error",
+  asyncHandler(async (_req, _res) => {
+    throw new AppError("This is a test error", 400);
+  })
+);
 
 export default router;
